83c836d1fc8be9a5ad2187ed9d254d5e87c1e96d
Priyadeep Sen <work.priyadeepsen@gmail.com>
2025-09-18 07:13:20 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Defining variables
+BACKUP_SRC="/home/subaru/Documents/Personal/"
+BACKUP_DST="/home/subaru/Backups"
+BACKUP_DATE=$(date +%Y%m%d%H%M%S)
+BACKUP_FILENAME="backup-demo-_$BACKUP_DATE.tar.gz"
+
+
+# Make directory
+mkdir -p "$BACKUP_DST/$BACKUP_DATE"
+
+# Compress & Archive src directory
+
+tar -czf "$BACKUP_DST/$BACKUP_DATE/$BACKUP_FILENAME" "$BACKUP_SRC"
+
+
+# Verify the backup that was created successfully.
+
+if [ $? -eq 0 ]; then
+	echo "Backup was successful: $BACKUP_FILENAME"
+else
+	echo "Backup Failed."
+fi
+
+
